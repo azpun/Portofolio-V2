7c119d8dbf7b224436e412a7fbe01289449edb36
azpun <azpunari@gmail.com>
2025-12-24 21:33:24 +0700

    feat: implement theme toggle functionality and update navigation layout

--- src/main.js
+++ src/main.js
@@ -1,12 +1,38 @@
-import './style.css'
+import './style.css';
 
 const btnMenu = document.getElementById('btnMenu');
 const navMenu = document.getElementById('navbarBurger')
+const btnTheme = document.getElementById('btnTheme');
+const rootElement = document.querySelector('html');
+const iconTheme = document.querySelector('#iconTheme');
+const iconMenu = document.querySelector('#iconMenu');
+
+
 
 btnMenu.addEventListener('click', () => {
     navMenu.classList.toggle('showMenu');
     navMenu.classList.toggle('hideMenu');
 });
+
+function toggleTheme() {
+    const currentTheme = rootElement.getAttribute('data-theme');
+    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+    rootElement.setAttribute('data-theme', newTheme);
+};
+
+function updateIconTheme() {
+    const currentTheme = rootElement.getAttribute('data-theme');
+    if (currentTheme === 'light') {
+        iconTheme.setAttribute('src', 'ui/black/sun-black.png');
+        iconMenu.setAttribute('src', 'ui/black/menu-black.png');
+    } else {
+        iconTheme.setAttribute('src', 'ui/white/moon-white.png');
+        iconMenu.setAttribute('src', 'ui/white/menu-white.png');
+    }
+};
+
+btnTheme.addEventListener('click', toggleTheme);
+btnTheme.addEventListener('click', updateIconTheme);
 
 // function setDataTheme() {
 //     const currentTheme = rootElement.getAttribute('data-theme');
